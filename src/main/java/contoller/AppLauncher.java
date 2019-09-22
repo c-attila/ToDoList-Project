5e73074c4778ba55e24bb0e2a92934e5e109b611
Attila Cornea <attila.cornea@gmail.com>
2019-09-22 16:22:04 +0200

    Maven projekt elkezdve JFX-szel együtt. gitignore hozzáadva

---
+++ src/main/java/contoller/AppLauncher.java
@@ -0,0 +1,13 @@
+package contoller;
+
+public class AppLauncher {
+
+    /**
+     * Launches the JavaFX application.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
