d6b1ed6e671fd58e6a0fd2e20129e84384ee8ad9
Attila Cornea <attila.cornea@gmail.com>
2019-09-22 16:49:37 +0200

    Log4j hozzáadva, developer tag elkezdve

--- src/main/java/contoller/Controller.java
+++ src/main/java/contoller/Controller.java
@@ -1,4 +1,10 @@
 package contoller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Controller {
+
+    private static Logger logger = LogManager.getLogger();
+
 }
