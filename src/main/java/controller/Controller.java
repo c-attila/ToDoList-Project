9e9c3f4a6c92b52ad2efa0aa9b04d7c6371b426d
Alex Andras Mandrik <alexvok12@gmail.com>
2019-09-27 14:06:55 +0200

    src\main\java\controller\Controller.java: showTodo most már az adatbázisból kéri le a teendőket. De valmiért nem éri el az adatbázist.
    src\main\resources\JFX\employeeScene.fxml: a showTodosAction a datePicker-hez lesz hozzárendelve, mivel ha nem onAction-be fut le a showTodosAction, akkor a VBox NullPointerExceptiont dob

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -8,6 +8,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.HBox;
@@ -24,6 +25,8 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -35,8 +38,13 @@ public class Controller implements Initializable {
     private Model model;
     private TodoDAO todoDAO;
 
+    private String userName;
+
     @FXML
     private VBox list_VBox;
+
+    @FXML
+    private DatePicker datePicker;
 
     @FXML
     private void adatbazisProbaAction(javafx.event.ActionEvent actionEvent) {
@@ -44,31 +52,17 @@ public class Controller implements Initializable {
     }
 
     @FXML
-    private void showTodosAction(){
-        java.util.Date today = new java.util.Date();
-        Todo todo = new Todo(new java.sql.Timestamp(today.getTime()), 'r', "It if sometimes furnished unwilling as addi", new java.sql.Timestamp(today.getTime()), "Aladár", false);
-        Todo todo2 = new Todo(new java.sql.Timestamp(today.getTime()), 'r',
-                "asd dasdasdasdasdsadasdsadsadsadsadsadsadsadasdsadsadsadsadscadasdsacsadsadsadsdsad" +
-                "dsadsadsadsadsadsadsadsdsadsdadsad" +
-                "dsadasdasdasdasd", new java.sql.Timestamp(today.getTime()), "Aladár", false);
-        Todo todo3 = new Todo(new java.sql.Timestamp(today.getTime()), 'r', " but advantage now him. Remark easily garret nor nay. Civil those mrs enjoy shy fat merry. You greatest jointure saw horrible. He pr", new java.sql.Timestamp(today.getTime()), "Aladár", false);
-        Todo todo4 = new Todo(new java.sql.Timestamp(today.getTime()), 'r', "se was justice new winding. In finished on he speaking suitable advanced if. Boy happiness sportsmen say prevaile", new java.sql.Timestamp(today.getTime()), "Aladár", false);
-        Todo todo5 = new Todo(new java.sql.Timestamp(today.getTime()), 'r', "antage now him. Remark easily garret nor nay. Civil those mrs enjoy shy fat merry. You greate", new java.sql.Timestamp(today.getTime()), "Aladár", false);
-        Todo todo6 = new Todo(new java.sql.Timestamp(today.getTime()), 'r', "It if sometimes furnished unwilling as addi", new java.sql.Timestamp(today.getTime()), "Aladár", false);
-
-
-        List<Todo> listTodo = new ArrayList<>();
-        listTodo.add(todo);
-        listTodo.add(todo2);
-        listTodo.add(todo3);
-        listTodo.add(todo4);
-        listTodo.add(todo5);
-        listTodo.add(todo6);
+    private void showTodosAction() {
+        LocalDate date = datePicker.getValue();
+        List<Todo> listTodo = TodoDAO.listTodo("id", userName, Timestamp.valueOf(date.atStartOfDay()));
+        logger.info(listTodo);
         showTodos(listTodo);
     }
 
 
-    public void login(Stage stage, Scene scene, String userName){
+    public void login(Stage stage, Scene scene, String userName) {
+
+        this.userName = userName;
 
         stage.setScene(scene);
 
@@ -80,53 +74,54 @@ public class Controller implements Initializable {
 
     }
 
-    private void showTodos(List<Todo> listTodo){
+    private void showTodos(List<Todo> listTodo) {
 
         int i = 0;
-        for(Todo todo : listTodo) {
+        if (listTodo != null)
+            for (Todo todo : listTodo) {
 
-            HBox hBox = new HBox();
-            if(i == 0) {
-                hBox.setStyle("-fx-padding: 8;");
-                i = 1;
-            }else {
-                hBox.setStyle("-fx-padding: 8; -fx-background-color: #429eb8;");
-                i = 0;
+                HBox hBox = new HBox();
+                if (i == 0) {
+                    hBox.setStyle("-fx-padding: 8;");
+                    i = 1;
+                } else {
+                    hBox.setStyle("-fx-padding: 8; -fx-background-color: #429eb8;");
+                    i = 0;
+                }
+                hBox.setSpacing(20);
+                hBox.setAlignment(Pos.CENTER_LEFT);
+
+                Rectangle rectangle = new Rectangle();
+                rectangle.setHeight(25);
+                rectangle.setWidth(25);
+                rectangle.setFill(Color.WHITE);
+                rectangle.setStroke(Color.rgb(153, 216, 234));
+                hBox.getChildren().add(rectangle);
+
+                Text text = new Text();
+                text.setText(todo.getId() + "." + todo.getDescription());
+                text.setStyle("-fx-font: 16 arial;");
+                text.setWrappingWidth(620);
+                hBox.getChildren().add(text);
+
+                Text text2 = new Text();
+                text2.setText(todo.getDeadline().toString());
+                text2.setStyle("-fx-font: 20 arial;");
+                text2.setWrappingWidth(100);
+                hBox.getChildren().add(text2);
+
+                Text text3 = new Text();
+                text3.setText(todo.getEmployee());
+                text3.setStyle("-fx-font: 20 arial;");
+                text3.setWrappingWidth(100);
+                hBox.getChildren().add(text3);
+
+                CheckBox checkBox = new CheckBox();
+                checkBox.setSelected(todo.isDone());
+                hBox.getChildren().add(checkBox);
+
+                list_VBox.getChildren().add(hBox);
             }
-            hBox.setSpacing(20);
-            hBox.setAlignment(Pos.CENTER_LEFT);
-
-            Rectangle rectangle = new Rectangle();
-            rectangle.setHeight(25);
-            rectangle.setWidth(25);
-            rectangle.setFill(Color.WHITE);
-            rectangle.setStroke(Color.rgb(153,216,234));
-            hBox.getChildren().add(rectangle);
-
-            Text text = new Text();
-            text.setText(todo.getId() + "." + todo.getDescription());
-            text.setStyle("-fx-font: 16 arial;");
-            text.setWrappingWidth(620);
-            hBox.getChildren().add(text);
-
-            Text text2 = new Text();
-            text2.setText(todo.getDeadline().toString());
-            text2.setStyle("-fx-font: 20 arial;");
-            text2.setWrappingWidth(100);
-            hBox.getChildren().add(text2);
-
-            Text text3 = new Text();
-            text3.setText(todo.getEmployee());
-            text3.setStyle("-fx-font: 20 arial;");
-            text3.setWrappingWidth(100);
-            hBox.getChildren().add(text3);
-
-            CheckBox checkBox = new CheckBox();
-            checkBox.setSelected(todo.isDone());
-            hBox.getChildren().add(checkBox);
-
-            list_VBox.getChildren().add(hBox);
-        }
 
 
     }
